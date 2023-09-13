6257fc08d94ce14bdc426315bd6799ed86c14fea
Willem de Langen <w.de.langen@home.nl>
2023-09-13 15:11:24 +0200

    WdL Basic Flask Code

---
+++ flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<p>Hello World!...</p>"
+
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
+
+
+#
+# git checkout -b main      Switched to a new branch 'main'
+# Er wordt een kopie gemaakt van de omgeving. Je kan nu met 2 omgevingen die de zelfde basis hebben verder werken.
+
+
+#
+#  Opstarten met   flask --app flask_app run
+#
